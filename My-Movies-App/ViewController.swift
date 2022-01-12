ba56f3cc3f49f8b0e24b94604c6771fbd0b6c621
Pongsatorn Ploypukdee <pongsatorn@buzzebees.com>
2022-01-12 16:50:45 +0700

    Initial Commit

---
+++ My-Movies-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-Movies-App
+//
+//  Created by BOICOMP21070027 on 12/1/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
